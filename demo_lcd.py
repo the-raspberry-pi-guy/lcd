1969fd55dcb915fd0e4251533814d63f5be508f8
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2015-08-20 20:17:58 +0100

    Update demo program!

--- demo_lcd.py
+++ demo_lcd.py
@@ -5,9 +5,20 @@
 import lcddriver
 from time import *
 
-# Load the driver and set it to "lcd"
-lcd = lcddriver.lcd()
+# Load the driver and set it to "display"
+# If you use something from the driver library use the "display." prefix first
+display = lcddriver.lcd()
 
-# Remember that your sentences can only be 16 characters long!
-lcd.lcd_display_string("Greetings Human!", 1) # Write line of text to first line of display
-lcd.lcd_display_string("Demo Pi Guy code", 2) # Write line of text to second line of display
+try:
+    while True:
+        # Remember that your sentences can only be 16 characters long!
+        print("Writing to your display!")
+        display.lcd_display_string("Greetings Human!", 1) # Write line of text to first line of display
+        display.lcd_display_string("Demo Pi Guy code", 2) # Write line of text to second line of display
+        time.sleep(5)
+        display.lcd_clear()
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("Cleaning up!")
+    display.lcd_clear()
