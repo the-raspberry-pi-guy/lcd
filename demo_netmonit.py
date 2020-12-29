98fdfaf2c7d6a61cb79f0e2f06749954e22c9d41
Carlos Gomes <me@cgomesu.com>
2020-12-28 22:01:10 -0300

    added services and netcat function

--- demo_netmonit.py
+++ demo_netmonit.py
@@ -18,12 +18,18 @@ def end(msg=None, status=0):
 
 
 # Returns True if host responds to a ping request within the timeout interval
-def ping(host, timeout):
+def ping(host, timeout=3):
     return call(['ping', '-c', '1', '-W', str(timeout), str(host)],
                 stdout=open(devnull, 'w'),
-                stdin=open(devnull, 'w')) == 0
+                stderr=open(devnull, 'w')) == 0
 
 
+# Returns True if host has given port open
+def nc(host, port, timeout=3):
+    return call(['nc', '-z', '-w', str(timeout), str(host), str(port)], stderr=open(devnull, 'w')) == 0
+
+
+# assumes that running 16x2 LCD
 def lcd_print(top=None, bottom=None, delay=5):
     display.lcd_clear()
     display.lcd_display_string('{:^16}'.format(top), 1)
@@ -46,11 +52,18 @@ def main():
                                                   check_output(['hostname', '-I']).split()[0]),
                   delay=10)
         while True:
+            # use ping for hosts
             for host, address in hosts.items():
                 lcd_print(top="# NetMonitor #",
-                          bottom="{} is UP".format(host) if ping(address, 4) else "{} is DOWN".format(host))
+                          bottom="{} is UP".format(host) if ping(address) else "{} is DOWN".format(host))
+            # use netcat for services
+            for service, address in services.items():
+                lcd_print(top="# NetMonitor #",
+                          bottom="{} is UP".format(service) if nc(address['ip'], address['port']) else "{} is DOWN".format(service))
     except KeyboardInterrupt:
         end('Signal to stop', 0)
+    except (RuntimeError, IOError):
+        end('I2C bus error', 1)
 
 
 if __name__ == "__main__":
@@ -60,8 +73,11 @@ if __name__ == "__main__":
     hosts = {
         'Internet': '8.8.8.8',
         'Firewall': '192.168.1.1',
-        'NAS': '192.168.1.2',
-        'Cameras': '192.168.1.3',
-        'Plex': '192.168.1.4'
+        'NAS': '192.168.1.2'
+    }
+    # customizable dict of unique, netcatable services
+    services = {
+        'Cameras': {'ip': '192.168.1.2', 'port': '8000'},
+        'Plex': {'ip': '192.168.1.2', 'port': '32400'}
     }
     main()
