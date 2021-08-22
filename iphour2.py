44298389f79ec0c88decadbb5fa6e15c0021ce7b
Juan Escobar <juandavid.escobar@gmail.com>
2021-08-22 13:29:40 -0500

    Fixing my stuff fixes fixes fixes... not quite there yet but at least it does what i want it to do

--- iphour2.py
+++ iphour2.py
@@ -1,6 +1,7 @@
 import drivers
 from datetime import date
 from datetime import time
+from datetime import datetime
 import time
 import get_ip
 import threading
@@ -35,9 +36,16 @@ def get_theysaidso_randomcat():
     Function that picks a random category from the category list
     It is used by the thread_get_theysaidso_qod
     '''
-    tss_list_cats=list(api_tss_catlist_json['contents']['categories'].items())
-    random_category=random.choice(tss_list_cats)
-    return random_category[0]
+    global api_tss_catlist_json
+    try:
+        print("picking a random from these:\n")
+        print(api_tss_catlist_json)
+        tss_list_cats=list(api_tss_catlist_json['contents']['categories'].items())
+        random_category=random.choice(tss_list_cats)
+        print("the random category is: " + random_category[0])
+        return random_category[0]
+    except KeyError:
+        print(api_tss_catlist_json['error']['message'])
 
 def thread_get_theysaidso_qod():
     '''
@@ -57,27 +65,31 @@ def thread_get_theysaidso_qod():
 def thread_get_dollar_conversion():
     ''' 
     This thread gets the 1 usd to cop conversion. 
-    Using Free Currency Convert (FCC) as first option, 
-    then ExchangeRate API (ERA) as fallback. 
-    First updates every hour, the other once every day. 
-    We are prioritizing FCC over ERA. 
+    Using ExchangeRate API (ERA) as first option. 
+    Using Free Currency Convert (FCC) as fallback, 
+    Even though FCC updates every hour, it is slow.
+    ERA updates once every day. 
+    We are prioritizing ERA over FCC. 
     '''
+    print(str(datetime.now()) + " " + "debug: starting the usd2cop request")
     while True:
         global disp_string_usd2cop_value
         try:
-            api_freeCurrConv_request=requests.get("https://free.currconv.com/api/v7/convert?q=USD_COP&compact=ultra&apiKey=a252cb255d5e022f74e3")
-            api_freeCurrConv_json=api_freeCurrConv_request.json()
-            disp_string_usd2cop_value="1USD:" + str(round(api_freeCurrConv_json['USD_COP'])) + "COP"
-            print("thr3_dollarconv got an update from FCC")
-            time.sleep(3600) 
-        except:
             api_ExchangeRateAPI_request=requests.get("https://v6.exchangerate-api.com/v6/e737393710effa1bc6705aa0/pair/USD/COP")
             api_ExchangeRateAPI_json=api_ExchangeRateAPI_request.json()
             disp_string_usd2cop_value="1USD:" + str(round(api_ExchangeRateAPI_json['conversion_rate'])) + "COP"
-            print("thr3_dollarconv got an update from ERA")
+            print(str(datetime.now()) + " " + "thr3_dollarconv got an update from ERA")
+            time.sleep(86400) 
+        except:
+            api_freeCurrConv_request=requests.get("https://free.currconv.com/api/v7/convert?q=USD_COP&compact=ultra&apiKey=a252cb255d5e022f74e3")
+            api_freeCurrConv_json=api_freeCurrConv_request.json()
+            disp_string_usd2cop_value="1USD:" + str(round(api_freeCurrConv_json['USD_COP'])) + "COP"
+            print(str(datetime.now()) + " " + "thr3_dollarconv got an update from FCC")
             time.sleep(3600) 
         finally:
-            disp_string_usd2cop_value="ERROR"
+            pass
+            # disp_string_usd2cop_value="ERROR"
+            # We have to find the possible errors that can happen. Remember the "finally:" section always runs, so putting ERROR in the variable is not appropriate
             
 def thread_get_weather_info():
     '''
@@ -157,22 +169,44 @@ if __name__=="__main__":
     thr2_get_tssqod=threading.Thread(target=thread_get_theysaidso_qod, daemon=True)
     thr3_dollarconv=threading.Thread(target=thread_get_dollar_conversion, daemon=True)
     thr4_weatherinfo=threading.Thread(target=thread_get_weather_info, daemon=True)
-    thr5_line1=threading.Thread(target=thread_line1, daemon=True)
-    thr6_line2=threading.Thread(target=thread_line2, daemon=True)
+    # thr5_line1=threading.Thread(target=thread_line1, daemon=True)
+    # thr6_line2=threading.Thread(target=thread_line2, daemon=True)
     
     # Then we start the threads that populate the variables
     thr1_catlist.start()
-    thr2_get_tssqod.start()
+
+    while api_tss_catlist_json == "":
+        # we wait for the categories info, then load thread 2
+        pass
+    else:
+        thr2_get_tssqod.start()
+
     thr3_dollarconv.start()
     thr4_weatherinfo.start()
+
+    # let's see what do we have
+    while disp_string_tss_quote == "" or disp_string_weatherInfo == "" or disp_string_usd2cop_value == "":
+        pass
+    else:
+        print("\nINFO TO DISPLAY:")
+        print(disp_string_tss_quote)
+        print(disp_string_usd2cop_value)
+        print(disp_string_weatherInfo + "\n")
+'''
+    while disp_string_tss_quote == "" or disp_string_weatherInfo == "" or disp_string_usd2cop_value == "":
+        print("waiting info...")
+    else:
+        print(disp_string_tss_quote + "\n")
+        print(disp_string_usd2cop_value + "\n")
+        print(disp_string_weatherInfo + "\n")
     
     # The display line 1 does not need api access, so we can start it right away
     # Before starting anything on line 2, we need to chech if the global variables are empty or not
     try:
-        ''' thread to control line 1 goes here '''
+        # thread to control line 1 goes here 
         thr5_line1.start()
 
-        ''' thread to control line 2 goes here '''
+        # thread to control line 2 goes here 
         while disp_string_tss_quote == "" or disp_string_weatherInfo == "" or disp_string_usd2cop_value == "":
             display.lcd_display_string("waiting info...", 2)
         else:
@@ -182,3 +216,4 @@ if __name__=="__main__":
     except KeyboardInterrupt:
         print("\nCleaning the display!")
         display.lcd_clear()
+'''
